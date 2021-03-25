3ef25331782d5a4b1952f0c2e00d76b730cb6a65
Parsleyka <grigorashnazar@gmail.com>
2021-03-25 12:45:52 +0200

    Telegram bot core has been created!

---
+++ Project/giftgenerator_bot/src/test/java/by/jrr/giftgenerator_bot/GiftgeneratorBotApplicationTests.java
@@ -0,0 +1,13 @@
+package by.jrr.giftgenerator_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftgeneratorBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
