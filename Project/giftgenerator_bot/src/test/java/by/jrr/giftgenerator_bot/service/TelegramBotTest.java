8d5c451ff322a379523383242d4bd71e6940b769
Parsleyka <grigorashnazar@gmail.com>
2021-03-28 23:10:27 +0300

    bot is prepared for testing

---
+++ Project/giftgenerator_bot/src/test/java/by/jrr/giftgenerator_bot/service/TelegramBotTest.java
@@ -0,0 +1,22 @@
+package by.jrr.giftgenerator_bot.service;
+
+import by.jrr.giftgenerator_bot.config.Mapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(classes = {TelegramBot.class, Mapper.class})
+class TelegramBotTest {
+
+    @Autowired
+    TelegramBot telegramBot;
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void onUpdateReceived() {
+    }
+}
